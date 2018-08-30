fe90bd22ac1b42ad15247dad566379d7e72adb93
Nguyen Tri Tuan Hiep <hiepnguyen@techx.com.vn>
2018-08-30 17:26:03 +0700

    common handle message from kafka

---
+++ build/src/modules/kafka/MessageHandler.d.ts
@@ -0,0 +1,18 @@
+/// <reference types="rx-core" />
+/// <reference types="rx-core-binding" />
+/// <reference types="rx-lite" />
+/// <reference types="rx-lite-aggregates" />
+/// <reference types="rx-lite-backpressure" />
+/// <reference types="rx-lite-coincidence" />
+/// <reference types="rx-lite-experimental" />
+/// <reference types="rx-lite-joinpatterns" />
+/// <reference types="rx-lite-time" />
+import { Observable } from "rx";
+import { IMessage } from "./types";
+declare type HandleResult = Observable<any> | boolean;
+declare type Handle = (msg: IMessage) => HandleResult;
+declare class MessageHandler {
+    handle(message: any, func: Handle): void;
+    getErrorMessage(error: Error): any;
+}
+export { MessageHandler, HandleResult, Handle, };
---
+++ build/src/modules/kafka/MessageHandler.js
@@ -0,0 +1,63 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const log_1 = require("../log");
+const SendRequest_1 = require("./SendRequest");
+const UriNotFound_1 = require("../errors/UriNotFound");
+class MessageHandler {
+    handle(message, func) {
+        try {
+            log_1.logger.info('Got message');
+            const msgString = message.value.toString();
+            log_1.logger.info(msgString);
+            const msg = JSON.parse(msgString);
+            const obs = func(msg);
+            if (obs === false) {
+                SendRequest_1.getInstance().sendResponse(msg.transactionId, null, msg.responseDestination.topic, msg.responseDestination.uri, this.getErrorMessage(new UriNotFound_1.default()));
+                return;
+            }
+            else if (obs === true) {
+                return;
+            }
+            obs.subscribe((data) => SendRequest_1.getInstance().sendMessage(msg.transactionId, msg.responseDestination.topic, msg.responseDestination.uri, { data: data }), (err) => {
+                log_1.logger.logError('error while processing request', err);
+                SendRequest_1.getInstance().sendResponse(msg.transactionId, null, msg.responseDestination.topic, msg.responseDestination.uri, this.getErrorMessage(err));
+            });
+        }
+        catch (e) {
+            log_1.logger.logError('error while processing message', e);
+        }
+    }
+    getErrorMessage(error) {
+        if (error['isSystemError']) {
+            if (error['source']) {
+                log_1.logger.logError('error', error['source']);
+            }
+            else {
+                log_1.logger.logError('error', error);
+            }
+            return {
+                status: {
+                    code: error.code,
+                    message: error.message,
+                    messageParams: error.messageParams,
+                    params: (error.params && error.params.length > 0) ? error.params : undefined,
+                }
+            };
+        }
+        else if (error['isForwardError']) {
+            return { status: error['status'] };
+        }
+        else {
+            log_1.logger.logError('error', error);
+            return {
+                status: {
+                    code: 'INTERNAL_SERVER_ERROR',
+                    message: 'an error has occurred',
+                }
+            };
+        }
+    }
+    ;
+}
+exports.MessageHandler = MessageHandler;
+//# sourceMappingURL=MessageHandler.js.map
