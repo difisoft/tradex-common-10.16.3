5fffb3e0dde67bb588143c5a8faeb1f8aa63bbaa
hiepntttechx <hiepnguyen@techx.com.vn>
2018-09-04 11:28:17 +0700

    rebuild

---
+++ build/src/modules/models/index.d.ts
@@ -0,0 +1,4 @@
+import IResponse, { createFailResponse, createSuccessResponse } from './IResponse';
+import IStatus from './IStatus';
+import IParamError from './IParamError';
+export { IResponse, createFailResponse, createSuccessResponse, IParamError, IStatus };
