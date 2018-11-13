bb807018ae57b1a8a446aeed60ba5d9d1853c5d6
cuongth <cuong.9312@gmail.com>
2018-11-13 18:19:07 +0700

    add new Utils

--- build/src/modules/utils/NumberUtils.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const round: (input: number, scale?: number) => number;
--- src/modules/utils/MathUtils.ts
+++ src/modules/utils/MathUtils.ts
@@ -1,5 +1,8 @@
+import { logger as Logger } from '../../modules/log';
+
 export const round = (input: number, scale: number = 2): number => {
   if (input === undefined) {
+    Logger.error('Can not rounding undefined number');
     throw new Error('Can not rounding undefined number');
   } else {
     return Number(input.toFixed(scale));
