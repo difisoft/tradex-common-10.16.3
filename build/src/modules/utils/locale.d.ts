e45ed7cd00a6b1f53ad8cfdfea0c23ae5e29b864
Hung Nguyen <nmhung109@techx.vn>
2018-12-27 08:12:01 +0700

    Update locale build

--- build/src/modules/utils/locale.d.ts
+++ build/src/modules/utils/locale.d.ts
@@ -2,6 +2,7 @@ import 'isomorphic-fetch';
 import { IStatus } from '../models';
 declare const getLanguageCode: (acceptLanguageHeader: string) => string;
 declare const init: (msNames: string, namespaceList: string[], requestTopic?: string, uri?: string) => void;
+declare const initInternal: (msNames: string, namespaceList: string[], requestTopic?: string, uri?: string) => void;
 declare const getInstance: () => any;
 declare const translateErrorMessage: (errorObject: IStatus, lang: string) => IStatus;
-export { getLanguageCode, init, getInstance, translateErrorMessage };
+export { getLanguageCode, init, initInternal, getInstance, translateErrorMessage };
