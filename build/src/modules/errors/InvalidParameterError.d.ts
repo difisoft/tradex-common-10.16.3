e6d88cda17901c571c0eb27b4c32b46a0d0cc28a
Hiep Nguyen <tuanhiep1232@gmail.com>
2019-03-06 17:28:23 +0700

    throwErr when params not empty in InvalidParameterError

--- build/src/modules/errors/InvalidParameterError.d.ts
+++ build/src/modules/errors/InvalidParameterError.d.ts
@@ -3,4 +3,5 @@ export default class InvalidParameterError extends GeneralError {
     constructor(params?: any);
     add: (code: any, fieldName: any, messageParams: any) => this;
     adds: (params: any) => this;
+    throwErr: () => void;
 }
