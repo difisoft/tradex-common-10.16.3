f4c66e0d419d90b03e413edaff1e226426edea6e
cuongth <cuong.9312@gmail.com>
2019-04-23 14:19:37 +0700

    fix method MathUtils.round (ignored if data is null or undefined)

--- build/src/modules/utils/MathUtils.js
+++ build/src/modules/utils/MathUtils.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const log_1 = require("../../modules/log");
 exports.round = (input, scale = 2) => {
-    if (input === undefined || input === null) {
+    if (input == null) {
         log_1.logger.warn('Warning rounding undefined/null number');
-        return 0;
+        return input;
     }
     else {
         return Number(input.toFixed(scale));
--- src/modules/utils/MathUtils.ts
+++ src/modules/utils/MathUtils.ts
@@ -1,9 +1,9 @@
 import { logger as Logger } from '../../modules/log';
 
 export const round = (input: number, scale: number = 2): number => {
-  if (input === undefined || input === null) {
+  if (input == null) {
     Logger.warn('Warning rounding undefined/null number');
-    return 0;
+    return input;
   } else {
     return Number(input.toFixed(scale));
   }
