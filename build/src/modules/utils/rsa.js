ad55efef9419c202e02ce3df8be6d7c591964ca9
Trần Hữu Cương <cuongth@techx.vn>
2020-05-15 08:31:26 +0000

    TRX-91: encrypt/decrypt password (using RSA_PKCS1_PADDING)

--- build/src/modules/utils/rsa.js
+++ build/src/modules/utils/rsa.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const crypto = require("crypto");
 const rsaEncrypt = (data, publicKey) => {
     const buffer = Buffer.from(data);
-    const encrypted = crypto.publicEncrypt(publicKey, buffer);
+    const encrypted = crypto.publicEncrypt({ key: publicKey, padding: 1 }, buffer);
     return encrypted.toString("base64");
 };
 exports.rsaEncrypt = rsaEncrypt;
 const rsaDecrypt = (data, privateKey) => {
     const buffer = Buffer.from(data, "base64");
-    const decrypted = crypto.privateDecrypt(privateKey, buffer);
+    const decrypted = crypto.privateDecrypt({ key: privateKey, padding: 1 }, buffer);
     return decrypted.toString("utf8");
 };
 exports.rsaDecrypt = rsaDecrypt;
--- src/modules/utils/rsa.ts
+++ src/modules/utils/rsa.ts
@@ -2,13 +2,13 @@ import * as crypto from "crypto";
 
 const rsaEncrypt = (data: string, publicKey: string) => {
   const buffer = Buffer.from(data);
-  const encrypted = crypto.publicEncrypt(publicKey, buffer);
+  const encrypted = crypto.publicEncrypt({key: publicKey, padding: 1}, buffer);
   return encrypted.toString("base64");
 };
 
 const rsaDecrypt = (data: string, privateKey: string) => {
   const buffer = Buffer.from(data, "base64");
-  const decrypted = crypto.privateDecrypt(privateKey, buffer);
+  const decrypted = crypto.privateDecrypt({key: privateKey, padding: 1}, buffer);
   return decrypted.toString("utf8");
 };
 
